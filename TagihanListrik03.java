789be03b5681a52f2ae4a2062605447d85d19f02
Athaa10 <athamaulidia0904@gmail.com>
2024-09-30 18:14:58 +0700

    commit jobsheet 3

---
+++ TagihanListrik03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TagihanListrik03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = 10;
+        a++;
+        a--;
+
+        System.out.println("Final result of a is: "+ ++a);
+
+    }
+}
